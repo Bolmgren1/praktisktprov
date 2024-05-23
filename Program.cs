21d14892bb9ce9995ed79754dbda3b4aa8b6d61f
Bolmgren1 <kebo05001@utb.vaxjo.se>
2024-05-23 16:37:16 +0200

    .

--- Program.cs
+++ Program.cs
@@ -5,34 +5,27 @@ using System.Reflection.Metadata.Ecma335;
 using praktisktprov;
 class Program
 {
-    List<Enemy> enemies;
-
-    static void Main()
-    {
-        Program program = new Program();
-        program.MainGameLoop();
-    }
-    public void MainGameLoop()
-    {
-        Player player = new Player("Player", 100, 5, 15); // Skapar spelaren
+    
+    static void Main(string[] args)
+    { 
+        Player player = new Player($"Player", 100, 5, 15); // skapar spelaren
         
-        enemies = new List<Enemy>();
+        List<Enemy> enemies = new List<Enemy> // skapar listan för fienden
         {
-            new Skelett();
-            new Hobbit();
-        }
-        
-        Console.Write(enemies.Count);
+            new Skelett(), // skapar fienden skelett
+            new Hobbit() // skapar fienden hobbit
+            
+        };
+
         int score = 0; // poängen startar på noll
 
-        Random random = new Random();
-        
-        while (player.Health > 0 && enemies[0].Health> 0)
+        Random random = new Random(); // skapar en random instans
+        while (player.Health > 0 && enemies.Count > 0) // när spelaren och fienden lever gäller denna loop
         {
-            
-            Console.WriteLine($"Du ska slåss mot {enemies[0].Name}");
+            Enemy currentEnemy = enemies[0]; // väljer första fienden i listan
+            Console.WriteLine($"Du ska slåss mot {currentEnemy.Name}"); // skriver ut vilken fiende jag ska slåss mot
 
-            while(player.Health > 0 && enemies[0].Health > 0) // Medans spelaren och fiende lever gäller detta
+            while(player.Health > 0 && currentEnemy.Health > 0) // Medans spelaren och fiende lever gäller detta
             {
                 Console.WriteLine("Det är spelarens tur, välj mellan alternativen under"); // skriver ut valen
                 Console.WriteLine("1. Attakera"); // väljer man 1 så attackerar man
@@ -41,16 +34,16 @@ class Program
                 string val = Console.ReadLine(); //läser om du väljer val 1 eller 2
                 if (val == "1") // när man väljer 1
                 {
-                    player.Attack(enemies[0]);// spelaren attakerar fiende
-                    if (enemies[0].Health > 0) // om fiende lever gäller detta
+                    player.Attack(currentEnemy);// spelaren attakerar fiende
+                    if (currentEnemy.Health > 0) // om fiende lever gäller detta
                     {
-                        enemies[0].Attack(player); //fiende attakerar spelare
+                        currentEnemy.Attack(player); //fiende attakerar spelare
                     }
                 }
                 else if (val == "2")  // om man valde val 2
                 {
                     Console.WriteLine($"{player.Name} Hp: {player.Health}"); //skriver ut spelarens liv
-                    Console.WriteLine($"{enemies[0].Name} Hp: {enemies[0].Health}"); // skriver ut fiendens liv
+                    Console.WriteLine($"{currentEnemy.Name} Hp: {currentEnemy.Health}"); // skriver ut fiendens liv
                 }
                 if (player.Health <= 0) // om spelarens liv är 0 eller under noll så händer detta 
                 {
@@ -58,18 +51,18 @@ class Program
                     score = 15; // poängen du får om du förlorar
                     break;
                 }
-                else if (enemies[0].Health <= 0) // om spelaren dödar fienden händer detta
+                else if (currentEnemy.Health <= 0) // om spelaren dödar fienden händer detta
                 {
                     Console.WriteLine($"Du vann då du döda fienden, du hade {player.Health} hp kvar!"); // Skriver ut spelaren liv och att du dödade fienden
             
                     score = random.Next(50, 250);// poängen du får för att vinna
-                    enemies.RemoveAt(0);
+                    enemies.RemoveAt(0); // tar bort fienden jag besegrat
                     
-                    Console.WriteLine(enemies[0].Health + "Vill du fortsätta (ja/nej)");
-                    string continueChoise = Console.ReadLine();
-                    if (continueChoise.ToLower()!= "ja")
+                    Console.WriteLine("Vill du fortsätta (ja/nej)"); // här kan man fortsätta mot nästa fiende
+                    string continueChoise = Console.ReadLine(); // läser spelaren val
+                    if (continueChoise.ToLower()!= "ja") // om valet inte är ja går detta igenom
                     {
-                        break;
+                        break; // alsutar loopen
                     }
                     
                 }  
@@ -77,9 +70,9 @@ class Program
             
         }
 
-        if (player.Health > 0 && enemies.Count == 0)
+        if (player.Health > 0 && enemies.Count == 0) // om spelaren är vid liv och fiende är inte vid liv
         {
-            Console.WriteLine("Du har dödat alla fiender");
+            Console.WriteLine("Du har dödat alla fiender"); 
         }
 
         Console.WriteLine($"Dina poäng är: {score}"); // Skriver ut dina poäng du får
@@ -94,6 +87,6 @@ class Program
         sw.WriteLine($"Ditt poäng är: {score}"); // Skriver ut ditt poäng i text filen
         sw.Close(); // stänger filen
     }
-
+    
 }
 
